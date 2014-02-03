50273be6b0b3cb401ef4636ebd8fb46ae602ef42
desu <desu@desu-pc>
2014-02-04 01:56:47 +0600

    initial

---
+++ src/main/java/ru/desu/home/isef/services/impl/MyServiceImpl.java
@@ -0,0 +1,30 @@
+package ru.desu.home.isef.services.impl;
+
+import ru.desu.home.isef.entity.Log;
+import ru.desu.home.isef.services.MyService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Service;
+
+@Service("myService")
+@Scope(value = "singleton", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class MyServiceImpl implements MyService {
+
+	@Autowired
+	LogDao dao;
+
+	public Log addLog(Log log) {
+		return dao.save(log);
+	}
+
+	public List<Log> getLogs() {
+		return dao.queryAll();
+	}
+
+	public void deleteLog(Log log) {
+		dao.delete(log);
+	}
+
+}
