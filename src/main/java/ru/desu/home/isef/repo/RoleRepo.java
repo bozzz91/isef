208d5685fd84a818e573a4ef91618787d806c5be
desu <desu@desu-home>
2014-02-10 13:11:01 +0600

    Репозиторий для ролей доделан

--- src/main/java/ru/desu/home/isef/repo/RoleRepo.java
+++ src/main/java/ru/desu/home/isef/repo/RoleRepo.java
@@ -1,10 +1,12 @@
 package ru.desu.home.isef.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.desu.home.isef.entity.Role;
 
 public interface RoleRepo extends JpaRepository<Role, Long> {
 
+    @Query("FROM #{#entityName} where lower(rolename) = lower(?1)")
     public Role findByRoleName(String name);
 
 }
