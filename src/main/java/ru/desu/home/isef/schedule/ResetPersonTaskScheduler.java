be14ff12f104e3086d4f650929f14a1c936562af
desu <bozzz91@gmail.com>
2015-07-22 18:33:02 +0500

    Добавлен шедулер очистки выполненных заданий через период, заданный в каждом задании (дефолт 24 часа)

---
+++ src/main/java/ru/desu/home/isef/schedule/ResetPersonTaskScheduler.java
@@ -0,0 +1,45 @@
+package ru.desu.home.isef.schedule;
+
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import ru.desu.home.isef.entity.PersonTask;
+import ru.desu.home.isef.entity.Task;
+import ru.desu.home.isef.repo.PersonTaskRepo;
+
+import javax.transaction.Transactional;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+@Log
+@Component
+@Transactional
+public class ResetPersonTaskScheduler {
+
+	@Autowired PersonTaskRepo ptRepo;
+
+	@Scheduled(fixedRate = 300000)
+	public void resetTasks() {
+		log.info("reset started");
+		int removed = 0;
+		List<PersonTask> pts = ptRepo.findAll();
+		for (PersonTask pt : pts) {
+			if (pt.getStatus() == 1) {
+				Calendar cal = Calendar.getInstance();
+				Task t = pt.getTask();
+				cal.add(Calendar.HOUR, -t.getPeriod());
+				Date expired = cal.getTime();
+				if (pt.getExecuted().before(expired)) {
+					ptRepo.delete(pt);
+					removed++;
+				}
+			}
+		}
+		if (removed > 0) {
+			log.info("removed " + removed + " executed tasks");
+		}
+	}
+
+}
